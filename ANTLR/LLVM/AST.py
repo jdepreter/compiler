1ffc3e2b03673ccf15ab48e681458194b032091e
jdepreter <jolan.depreter@gmail.com>
2020-02-18 13:22:43 +0100

    Base AST Class

---
+++ ANTLR/LLVM/AST.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self):
+        ...
+
+
+class Operator(Node):
+    def __init__(self):
+        Node.__init__(self, )
+
+
+class AST:
+    def __init__(self):
+        ...
