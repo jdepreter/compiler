2804a5dc780f7aa45db7675b4cc38c712e70b919
Quentindh <quentindehaes@outlook.com>
2020-03-12 14:10:35 +0100

    added start 4 bool methodic in llvm

--- ANTLR/LLVM/LLVM_CONVERTER.py
+++ ANTLR/LLVM/LLVM_CONVERTER.py
@@ -4,15 +4,33 @@ class LLVM_Converter:
         self.stack = []
         self.register = 0
         self.file = file
-        self.format_dict = {'int': 'i32', 'float': 'float'}
-        self.optype ={'+':{'int':'add', 'float': 'fadd'},
-                      '-':{'int':'sub', 'float': 'fsub'},
-                      '*': {'int':'mul', 'float': 'fmul'},
-                      '/':{'int':'sdiv', 'float': 'fdiv'},
-                      '%': {'int':'srem', 'float': 'frem'}}
+        self.format_dict = {'int': 'i32', 'float': 'f32'}
+        self.optype ={'+': {'int': 'add', 'float': 'fadd'},
+                      '-': {'int': 'sub', 'float': 'fsub'},
+                      '*': {'int': 'mul', 'float': 'fmul'},
+                      '/': {'int': 'sdiv', 'float': 'fdiv'},
+                      '%': {'int': 'srem', 'float': 'frem'}}
+
+        self.bool_dict = {'int':
+                              {
+                                  '==': 'icmp eq',
+                                  '!=': 'icmp ne',
+                                  '>': 'icmp sgt',
+                                  '<': 'icmp slt',
+                                  '>=': 'icmp sge',
+                                  '<=': 'icmp sle'},
+                          'float':
+                              {
+                                  '==': 'fcmp oeq',
+                                  '!=': 'fcmp ole',
+                                  '>': 'fcmp ogt',
+                                  '<': 'fcmp olt',
+                                  '>=': 'fcmp oge',
+                                  '<=': 'fcmp ole'
+                              }
+                          }
 
     def to_llvm(self):
-        # self.stack.insert(0, self.ast.startnode)
         current_symbol_table = self.ast.startnode.symbol_table
         self.solve_llvm_node(self.ast.startnode, current_symbol_table)
 
@@ -20,14 +38,13 @@ class LLVM_Converter:
     # helpermethod to write used for declaration or definition
     def allocate_node(self, node, symbol_table):
         reg_nr = symbol_table.get_symbol(str(node.children[1].label), None).current_register
-        self.file.write("%a" + str(reg_nr) + " = alloca "
-                         + self.format_dict[str(node.children[0].label)] + "\n")
+        self.file.write("%a" + str(reg_nr) + " = alloca " + self.format_dict[str(node.children[0].label)] + "\n")
 
         return "%a" + str(reg_nr)
 
-
     def solve_llvm_node(self, node, symbol_table):
         # TODO x++ & ++x staan nog ni ok in den boom && add char / double && maybe arrays && typeswitching + warnings
+        # TODO fix bools in llvm
         if node.symbol_table != None:
             symbol_table = node.symbol_table
 
@@ -65,7 +82,7 @@ class LLVM_Converter:
 
     def solve_math(self, node, symbol_table, type):
         string = ''
-        if node.label in ['+','-','*','/', '%']:
+        if node.label in ['+', '-', '*', '/', '%']:
             reg = self.register
             self.register += 1
             string = '%r' + str(reg) + ' = '+self.optype[node.label][type] +' ' + self.format_dict[type] +' '+ \
@@ -75,8 +92,29 @@ class LLVM_Converter:
             return '%r' + str(reg)
 
         elif node.label == '&&':
-            ...
+            reg = self.register
+            self.register += 1
+            string = '%r' + str(reg) + ' = and i32 ' + \
+                     self.solve_math(node.children[0], symbol_table, type) + ', ' + self.solve_math(node.children[1],
+                                                                                                    symbol_table,
+                                                                                                    type) + '\n'
+
+            self.file.write(string)
+            return '%r' + str(reg)
         elif node.label == '||':
+            reg = self.register
+            self.register += 1
+            string = '%r' + str(reg) + ' = or i32 ' + \
+                     self.solve_math(node.children[0], symbol_table, type) + ', ' + self.solve_math(node.children[1],
+                                                                                                    symbol_table,
+                                                                                                    type) + '\n'
+
+            self.file.write(string)
+            return '%r' + str(reg)
+
+
+        if node.label in ['==', '!=', '<', '>', '<=', '>=']:
+
             ...
         else:
             try:
