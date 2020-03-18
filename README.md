5a810595a4228bc36c6997f3e88094d5d73d15c7
jdepreter <jolan.depreter@gmail.com>
2020-03-18 14:14:52 +0100

    unique binary output

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Also make sure graphviz is installed on your system for AST dot output.
 
 ### Testing
 `python run_tests.py`
+
+For testing syntax errors:
+- syntax_error.txt
+- syntax_error_1.txt
+
--- tests/test_base.py
+++ tests/test_base.py
@@ -5,29 +5,29 @@ from CustomExceptions import CSyntaxError
 
 class TestCase(unittest.TestCase):
     def test_basic_files(self):
-        to_llvm("basic_declaration.txt", "basic_declaration.txt")
-        to_llvm("basic_definition.txt", "basic_definition.txt")
+        to_llvm("basic_declaration.txt", "basic_declaration")
+        to_llvm("basic_definition.txt", "basic_definition")
 
     def test_scope(self):
         to_llvm("scope_1.txt", "scope_1.txt")
-        to_llvm("scope_empty.txt", "scope_empty.txt")
-        to_llvm("scope_nested.txt", "scope_nested.txt")
+        to_llvm("scope_empty.txt", "scope_empty")
+        to_llvm("scope_nested.txt", "scope_nested")
 
     def test_int_folding(self):
         to_llvm("folding.txt", "folding.txt")
 
     def test_bool_folding(self):
-        to_llvm("bool_testing.txt", "bool_testing.txt")
+        to_llvm("bool_testing.txt", "bool_testing")
 
     def test_pointers(self):
-        to_llvm("pointers.txt", "bool_testing.txt")
+        to_llvm("pointers.txt", "bool_testing")
 
     def test_errors(self):
         with self.assertRaises(CSyntaxError):
-            to_llvm("syntax_error.txt", "syntax_error.txt")
+            to_llvm("syntax_error.txt", "syntax_error")
 
         with self.assertRaises(CSyntaxError):
-            to_llvm("syntax_error_1.txt", "syntax_error_1.txt")
+            to_llvm("syntax_error_1.txt", "syntax_error_1")
 
 
 if __name__ == '__main__':
